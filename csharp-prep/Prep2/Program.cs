a7065c5cdb80aa7ccb7d42fde7ccace965029029
EthanLMW <csctrmyung@gmail.com>
2024-01-12 21:43:03 -0700

    week 1 prep 1-2 completed

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.Numerics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        int grade = int.Parse(Console.ReadLine());
+        string letter = "";
+
+        if (grade >= 90) {
+            letter = "A";
+        }
+
+        else if (grade >= 80) {
+            letter = "B";
+        }
+
+        else if (grade >= 70) {
+            letter = "C";
+        }
+
+        else if (grade >= 60) {
+            letter = "D";
+        }
+
+        else if (grade < 60) {
+            letter = "F";
+        }
+
+        Console.Write($"Your grade is {letter}. ");
+
+        if (grade >= 70) {
+            Console.Write("You passed the class.");
+        }
+
+        else {
+            Console.Write("You didn't passed the class.");
+        }
     }
 }
